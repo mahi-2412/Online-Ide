fddf75cafb344f278fa45d03b96491337a4a64f0
haribabu87 <149959941+haribabu87@users.noreply.github.com>
2024-03-23 16:46:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var compiler = require("compilex");
+
+var app = express();
+app.use(bodyParser());
+
+var option = { stats: true };
+compiler.init(option);
+
+app.set('view engine', 'ejs');
+app.get("/compilecode", function (req, res) {
+  const data = {
+    code : '',
+    output:'',
+    input: ''
+  }
+    res.render('index',data);
+});
+
+app.post("/compilecode", function (req, res) {
+    var code = req.body.code;
+    var input = req.body.input;
+    var inputRadio = req.body.inputRadio;
+    var lang = req.body.lang;
+    const values = {
+        code : code,
+        output:'',
+        input: input
+      }
+    if (lang === "C" || lang === "C++") {
+        if (inputRadio === "true") {
+            var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
+            compiler.compileCPPWithInput(envData, code, input, function (data) {
+                if (data.error) {
+                    res.send(data.error);
+                } else {
+                    res.send(data.output);
+                }
+            });
+        } else {
+            var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
+            compiler.compileCPP(envData, code, function (data) {
+                res.send(data);
+
+                //data.error = error message
+                //data.output = output value
+            });
+        }
+    }
+    if (lang === "Python") {
+        if (inputRadio === "true") {
+            var envData = { OS: "windows" };
+            compiler.compilePythonWithInput(envData, code, input, function (data) {
+                res.send(data);
+            });
+        } else {
+            var envData = { OS: "windows" };
+            compiler.compilePython(envData, code, function (data) {
+               values.output = data.output;
+               res.render('index',values);
+            });
+        }
+    }
+});
+
+app.get("/fullStat", function (req, res) {
+    compiler.fullStat(function (data) {
+        res.send(data);
+    });
+});
+
+app.listen(7000);
+
+compiler.flush(function () {
+    console.log("All temporary files flushed !");
+});
